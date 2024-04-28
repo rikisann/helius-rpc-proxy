ffa5a6a795ab78632f03037484ae22b6e92529ad
rikisann <ricardito3053@gmail.com>
2024-04-28 17:42:34 -0400

    changed to alchemy rpc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 interface Env {
 	CORS_ALLOW_ORIGIN: string;
-	HELIUS_API_KEY: string;
+	ALCHEMY_API_KEY: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env) {
-
 		// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
 		// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
 		// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
@@ -14,48 +13,52 @@ export default {
 		// header to `https://example.com`. Multiple domains are supported by verifying that the request
 		// originated from one of the domains in the `CORS_ALLOW_ORIGIN` environment variable.
 		const supportedDomains = env.CORS_ALLOW_ORIGIN ? env.CORS_ALLOW_ORIGIN.split(',') : undefined;
+
 		const corsHeaders: Record<string, string> = {
-			"Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, OPTIONS",
-			"Access-Control-Allow-Headers": "*",
-		}
+			'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
+			'Access-Control-Allow-Headers': '*',
+		};
 		if (supportedDomains) {
-			const origin = request.headers.get('Origin')
+			const origin = request.headers.get('Origin');
+			console.log(origin);
+
 			if (origin && supportedDomains.includes(origin)) {
-				corsHeaders['Access-Control-Allow-Origin'] = origin
+				corsHeaders['Access-Control-Allow-Origin'] = origin;
 			}
 		} else {
-			corsHeaders['Access-Control-Allow-Origin'] = '*'
+			corsHeaders['Access-Control-Allow-Origin'] = '*';
 		}
 
-		if (request.method === "OPTIONS") {
+		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 200,
 				headers: corsHeaders,
 			});
 		}
 
-		const upgradeHeader = request.headers.get('Upgrade')
+		// const upgradeHeader = request.headers.get('Upgrade')
 
-		if (upgradeHeader || upgradeHeader === 'websocket') {
-			return await fetch(`https://mainnet.helius-rpc.com/?api-key=${env.HELIUS_API_KEY}`, request)
-		}
+		// if (upgradeHeader || upgradeHeader === 'websocket') {
+		// 	return await fetch(`https://https://solana-mainnet.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`, request)
+		// }
 
-
-		const { pathname, search } = new URL(request.url)
+		const { pathname } = new URL(request.url);
 		const payload = await request.text();
-		const proxyRequest = new Request(`https://${pathname === '/' ? 'mainnet.helius-rpc.com' : 'api.helius.xyz'}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
-			method: request.method,
-			body: payload || null,
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Helius-Cloudflare-Proxy': 'true',
+		const proxyRequest = new Request(
+			`https://solana-mainnet.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
+			{
+				method: request.method,
+				body: payload || null,
+				headers: {
+					'Content-Type': 'application/json',
+				},
 			}
-		});
+		);
 
 		return await fetch(proxyRequest).then(res => {
 			return new Response(res.body, {
 				status: res.status,
-				headers: corsHeaders
+				headers: corsHeaders,
 			});
 		});
 	},
